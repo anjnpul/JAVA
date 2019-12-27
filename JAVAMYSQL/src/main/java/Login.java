b504010ab52b92f776a13782aca2f6ec11086019
ANJAN <anjanvkp@gmail.com>
2019-12-28 03:06:11 +0530

    JAVAMYSQL

---
+++ JAVAMYSQL/src/main/java/Login.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author Anjan
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Connection conn=null;
+            conn = MYSQL_CONNECT.ConnectDB();
+            try
+           {
+               String _userName = request.getParameter("user");
+               String _passWord = request.getParameter("pass");
+               if (_userName != null)
+               {
+                   String Query = "select * from user where user=? and pass=?";
+                   PreparedStatement psm = conn.prepareStatement(Query);
+                   psm.setString (1, _userName);
+                   psm.setString(2, _passWord);
+                   ResultSet rs=psm.executeQuery();
+                   if (rs.next())
+                   {
+                       response.sendRedirect("Member_Home.html");
+                   }
+                   else{
+                       out.println("Login Failed! Please try again");
+                   }
+               }
+           }
+           catch (Exception ex){
+               out.println(ex.getMessage());
+           }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
