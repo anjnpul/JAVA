4323454cacbd9c847a521c5aa166e36437b144d6
ANJAN <anjan.paul3@gmail.com>
2019-12-31 18:59:34 +0530

    dialog

--- DATAENTRY/src/main/java/servlets/SaveData.java
+++ DATAENTRY/src/main/java/servlets/SaveData.java
@@ -26,34 +26,42 @@ public class SaveData extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        String REF = req.getParameter("REF");
-        String DATE = req.getParameter("DATE");
-        String ACC = req.getParameter("ACC");
-        String ACCTP = req.getParameter("ACCTP");
-        String PARTY = req.getParameter("PARTY");
-        String PARTYTP = req.getParameter("PARTYTP");
-        String AMT = req.getParameter("AMT");
-        String REM = req.getParameter("REM");
-        String PROJ = req.getParameter("PROJ");
-        
-        data e = new data();
-        e.setREF(REF);
-        e.setDATE(DATE);
-        e.setACC(ACC);
-        e.setACCTP(ACCTP);
-        e.setPARTY(PARTY);
-        e.setPARTYTP(PARTYTP);
-        e.setAMT(AMT);
-        e.setREM(REM);
-        e.setPROJ(PROJ);
-        int status=DataAccess.save(e);
-        String msg = "ANJAN";
-        if(status>0){  
-            msg = "Data Saved Successfully";  
-        }else{  
-            msg = "Error in Data Saving";  
-        }   
-        resp.getWriter().write(msg);
-    }
+        String ACT = req.getParameter("ACT");
 
+        if (ACT.equals("ADD")) {
+            String REF = req.getParameter("REF");
+            String DATE = req.getParameter("DATE");
+            String ACC = req.getParameter("ACC");
+            String ACCTP = req.getParameter("ACCTP");
+            String PARTY = req.getParameter("PARTY");
+            String PARTYTP = req.getParameter("PARTYTP");
+            String AMT = req.getParameter("AMT");
+            String REM = req.getParameter("REM");
+            String PROJ = req.getParameter("PROJ");
+
+            data e = new data();
+            e.setREF(REF);
+            e.setDATE(DATE);
+            e.setACC(ACC);
+            e.setACCTP(ACCTP);
+            e.setPARTY(PARTY);
+            e.setPARTYTP(PARTYTP);
+            e.setAMT(AMT);
+            e.setREM(REM);
+            e.setPROJ(PROJ);
+            int status = DataAccess.save(e);
+            String msg = "ANJAN";
+            if (status > 0) {
+                msg = "Data Saved Successfully";
+            } else {
+                msg = "Error in Data Saving";
+            }
+            resp.getWriter().write(msg);
+        }
+        
+        if (ACT.equals("EDIT")) {
+            String msg = "ANJAN";
+            resp.getWriter().write(msg);
+        }
+    }
 }
